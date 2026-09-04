2c525cae2690c3b036fec42535797c26534c4bd1
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-03 21:25:02 -0700

    fix: add example of the config service get

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT', 3000);
+  const databaseUrl = configService.get<string>('DATABASE_URL');
+
+  // TODO: Remove temporary DATABASE_URL startup log once SQLite is onboarded, this is just an example of using the config service, and getting stuff from "outside" of nest
+  logger.log(`DATABASE_URL: ${databaseUrl}`);
+
+  await app.listen(port);
+  logger.log(`Application is running on port: ${port}`);
 }
 void bootstrap();
