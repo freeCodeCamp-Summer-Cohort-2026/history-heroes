f37f2f3bf2e7606616c272cd9161834a4c17ace6
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-03 21:30:29 -0700

    fix: add helmet configuration

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   const configService = app.get(ConfigService);
 
