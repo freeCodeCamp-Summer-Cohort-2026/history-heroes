f74e25ea581f0389a17410ab1af0e43fe87b01cf
Lore <lorevandenheede@gmail.com>
2026-09-09 19:52:09 +0200

    chore(client): set up vitest, RTL, jsdom and example test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,6 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
+import { defineConfig } from 'vitest/config'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -14,5 +13,10 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
   },
 })
