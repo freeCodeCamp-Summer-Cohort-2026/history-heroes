1259eaab19daf49f5f493f0a9f2341a0d30016d2
shy_away <149800599+shy-away@users.noreply.github.com>
2026-09-11 17:05:14 -0500

    style(api): prettier formatting

--- server/src/core/db/seeds/schemas/activities-seed.schema.ts
+++ server/src/core/db/seeds/schemas/activities-seed.schema.ts
@@ -1,50 +1,71 @@
 import { z } from 'zod';
 
-const setsIdenticalAndNoEmptyStrings = (a: Set<string>, b: Set<string>): boolean => {
+const setsIdenticalAndNoEmptyStrings = (
+  a: Set<string>,
+  b: Set<string>,
+): boolean => {
   if (a.size !== b.size) return false;
 
   for (const aVal of a.values()) {
-    if (aVal === "" || !b.has(aVal)) return false;
+    if (aVal === '' || !b.has(aVal)) return false;
   }
 
   return true;
-}
+};
 
-const Ordering = z.object({
-  type: z.literal('ordering'),
-  content: z.object({
-    items: z.array(z.object({ id: z.string(), label: z.string() })).nonempty(),
-  }),
-  successCriteria: z.object({
-    correctOrder: z.array(z.string()).nonempty(),
-  }),
-}).refine(({ content, successCriteria }): boolean => {
-  const contentIds = new Set(content.items.map(item => item.id));
-  const successIds = new Set(successCriteria.correctOrder);
+const Ordering = z
+  .object({
+    type: z.literal('ordering'),
+    content: z.object({
+      items: z
+        .array(z.object({ id: z.string(), label: z.string() }))
+        .nonempty(),
+    }),
+    successCriteria: z.object({
+      correctOrder: z.array(z.string()).nonempty(),
+    }),
+  })
+  .refine(({ content, successCriteria }): boolean => {
+    const contentIds = new Set(content.items.map((item) => item.id));
+    const successIds = new Set(successCriteria.correctOrder);
 
-  return setsIdenticalAndNoEmptyStrings(contentIds, successIds);
-});
+    return setsIdenticalAndNoEmptyStrings(contentIds, successIds);
+  });
 
-const Matching = z.object({
-  type: z.literal('matching'),
-  content: z.object({
-    left: z.array(z.object({ id: z.string(), label: z.string() })).nonempty(),
-    right: z.array(z.object({ id: z.string(), label: z.string() })).nonempty(),
-  }),
-  successCriteria: z.object({
-    pairs: z.array(
-      z.object({
-        left: z.string(),
-        right: z.string(),
-      }),
-    ).nonempty(),
-  }),
-}).refine(({ content, successCriteria }) => {
-  const contentIds = new Set([...content.left.map(e => e.id), ...content.right.map(e => e.id)]);
-  const successIds = new Set(successCriteria.pairs.reduce((acc: string[], e) => [...acc, e.left, e.right], []));
+const Matching = z
+  .object({
+    type: z.literal('matching'),
+    content: z.object({
+      left: z.array(z.object({ id: z.string(), label: z.string() })).nonempty(),
+      right: z
+        .array(z.object({ id: z.string(), label: z.string() }))
+        .nonempty(),
+    }),
+    successCriteria: z.object({
+      pairs: z
+        .array(
+          z.object({
+            left: z.string(),
+            right: z.string(),
+          }),
+        )
+        .nonempty(),
+    }),
+  })
+  .refine(({ content, successCriteria }) => {
+    const contentIds = new Set([
+      ...content.left.map((e) => e.id),
+      ...content.right.map((e) => e.id),
+    ]);
+    const successIds = new Set(
+      successCriteria.pairs.reduce(
+        (acc: string[], e) => [...acc, e.left, e.right],
+        [],
+      ),
+    );
 
-  return setsIdenticalAndNoEmptyStrings(contentIds, successIds);
-});
+    return setsIdenticalAndNoEmptyStrings(contentIds, successIds);
+  });
 
 const ActivityTypes = z.discriminatedUnion('type', [Ordering, Matching]);
 
