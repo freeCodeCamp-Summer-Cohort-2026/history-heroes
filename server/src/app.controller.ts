a80cbe6f1cf7767adb924d88abcbc470ddb7b44b
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-10 22:04:11 -0700

    fix(84): add catch-all route
    - changed module resolution strategy and added extra opx based on the docs

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -7,11 +7,25 @@ export class AppController {
 
   /**
    * Example endpoint, can be loaded from:
-   * https://localhost:3000/api/v1/hello-world
+   * http://localhost:3000/api/v1/hello-world
    *
    */
   @Get('hello-world')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  /**
+   * Catch all route if nothing else matches, we return 404
+   *
+   * **note** this "catchall" is required, but is meaningless (blame express 5)
+   * ref:  https://docs.nestjs.com/controllers#route-wildcards
+   */
+  @Get('*catchall')
+  getNotFound() {
+    return {
+      statusCode: 404,
+      message: 'Not Found',
+    };
   }
 }
