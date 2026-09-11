f457562910bcf61864b48dbae3627dc6b12f69d6
shy_away <149800599+shy-away@users.noreply.github.com>
2026-09-11 16:38:39 -0500

    chore(api): test id refinement

--- server/src/core/db/seeds/schemas/activities-seed.schema.spec.ts
+++ server/src/core/db/seeds/schemas/activities-seed.schema.spec.ts
@@ -29,6 +29,98 @@ describe('Activity item schema', () => {
     expect(result.success).toBeFalsy();
     expect(result.error).toBeDefined();
   });
+
+  describe('should be invalid against an ordering activity with various invalid id configurations', () => {
+    let dummyOrderingActivity: {
+      id: string;
+      type: string;
+      title: string;
+      checkStatement: string;
+      content: {
+        items: { id: string; label: string }[];
+      };
+      successCriteria: {
+        correctOrder: string[];
+      };
+    };
+
+    const expectValidationFailure = () => {
+      const result = ActivitySeedItemSchema.safeParse(dummyOrderingActivity);
+
+      expect(result.success).toBeFalsy();
+      expect(result.error).toBeDefined();
+    };
+
+    // reset dummy activity before each test
+    beforeEach(() => {
+      dummyOrderingActivity = {
+        id: 'dummy-ordering-activity',
+        type: 'ordering',
+        title: 'Dummy ordering activity',
+        checkStatement: 'This is a dummy ordering activity',
+        content: {
+          items: [
+            {
+              id: 'first',
+              label: 'First',
+            },
+            {
+              id: 'second',
+              label: 'Second',
+            },
+          ],
+        },
+        successCriteria: {
+          correctOrder: ['first', 'second'],
+        },
+      };
+    });
+
+    it('shoudld be valid for unmodified dummy activity', () => {
+      ActivitySeedItemSchema.parse(dummyOrderingActivity);
+    });
+
+    it('should be invalid when there are too many successCriteria ids', () => {
+      dummyOrderingActivity.successCriteria.correctOrder.push('third');
+      expectValidationFailure();
+    });
+
+    it('should be invalid when there are not enough successCriteria ids', () => {
+      dummyOrderingActivity.successCriteria.correctOrder.pop();
+      expectValidationFailure();
+    });
+
+    it('should be invalid when successCriteria ids do not match', () => {
+      dummyOrderingActivity.successCriteria.correctOrder.splice(1, 1, 'third');
+      expectValidationFailure();
+    });
+
+    const dummyThirdContentItem = { id: 'third', label: 'Third' };
+
+    it('should be invalid when there are too many content ids', () => {
+      dummyOrderingActivity.content.items.push(dummyThirdContentItem);
+      expectValidationFailure();
+    });
+
+    it('should be invalid when there are not enough content ids', () => {
+      dummyOrderingActivity.content.items.pop();
+      expectValidationFailure();
+    });
+
+    it('should be invalid when content ids do not match', () => {
+      dummyOrderingActivity.content.items.splice(1, 1, dummyThirdContentItem);
+      expectValidationFailure();
+    });
+
+    it('should be invalid when there are matching empty ids', () => {
+      dummyOrderingActivity.successCriteria.correctOrder.splice(1, 1, '');
+      dummyOrderingActivity.content.items.splice(1, 1, {
+        id: '',
+        label: 'Empty',
+      });
+      expectValidationFailure();
+    });
+  });
 });
 
 describe('Activities file schema', () => {
