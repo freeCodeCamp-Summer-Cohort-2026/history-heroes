254d2fa79126fe828c3ddd632d6e7f0f1484e798
kaymade <kaymhelto@gmail.com>
2026-09-12 01:16:20 -0400

    feat(client): render module catalog list with selection

--- client/src/features/module/ModuleCatalog.tsx
+++ client/src/features/module/ModuleCatalog.tsx
@@ -1,31 +1,27 @@
-// Goal of this file is to recieve the fetch data from ModuleCatalogPage and render it
-// Imports Here
-import ModuleSummary from "./model/ModuleSummary.ts";
+import type { ModuleSummary } from './model/ModuleSummary'
 
-// Recieves modules and onModuleSelect 
-// Then defines varaibles 
 type ModuleCatalogProps = {
-    modules: ModuleSummary[];
-    onModuleSelect: (id: string) => void
+  modules: ModuleSummary[]
+  onModuleSelect: (id: string) => void
 }
 
-// Recieves the props object
-// Loops though the props object and makes a list
-// Use those values than return JSX that should appear 
-// Calls when clicked
-function ModuleCatalog(props: ModuleCatalogProps) {
-    const listModules = props.modules.map(oneModule => 
-    <li key={oneModule.id}>
-        <h2>{oneModule.title}</h2>
-        <p>{oneModule.description}</p>
-        <button onClick={props.onModuleSelect}>
-             Start Learning!
-        </button>
-        </li>)
-    return (
-        <ul>
-            {listModules}
-        </ul>
-    );
+export default function ModuleCatalog({
+  modules,
+  onModuleSelect,
+}: ModuleCatalogProps) {
+  return (
+    <ul>
+      {modules.map((oneModule) => (
+        <li key={oneModule.id}>
+          <h2>{oneModule.title}</h2>
+          <p>{oneModule.description}</p>
+          {oneModule.period && <p>{oneModule.period}</p>}
+          {oneModule.theme && <p>{oneModule.theme}</p>}
+          <button type="button" onClick={() => onModuleSelect(oneModule.id)}>
+            Start Learning!
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
 }
-
