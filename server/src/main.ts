87681bcebf0d1af57ee09b57c4b325084f047e32
River <river.karnas@gmail.com>
2026-09-11 21:27:37 -0300

    docs: import SwaggerModule, DocumentBuilder, NestFactory and setup DocumentBuilder

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,22 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('History Heroes API')
+    .setDescription('History Heroes API description')
+    .setVersion('1.0')
+    .addTag('history-heroes')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.use(helmet());
   app.setGlobalPrefix('api/v1');
