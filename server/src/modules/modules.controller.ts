0dee13b49f7056321cbdfa63bab26e71a27bfd2b
River <river.karnas@gmail.com>
2026-09-11 22:51:25 -0300

    docs(api): document the modules response shape

--- server/src/modules/modules.controller.ts
+++ server/src/modules/modules.controller.ts
@@ -1,6 +1,28 @@
 import { Controller, Get } from '@nestjs/common';
 import { ModulesService } from './modules.service';
 import { Module } from './entities/module.entity';
+import { ApiResponse } from '@nestjs/swagger';
+import { ApiProperty } from '@nestjs/swagger';
+
+class ModulesDto{
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty()
+  order: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;  
+}
 
 @Controller('modules')
 export class ModulesController {
@@ -12,6 +34,7 @@ export class ModulesController {
    * This can be loaded at: https://localhost:3000/api/v1/modules
    */
   @Get()
+  @ApiResponse({ type: ModulesDto })
   public findAll(): Promise<Module[]> {
     return this.modulesService.findAll();
   }
