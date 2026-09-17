bf1bb928c8e38bbc502cac89dd5db1290e2c66d6
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-16 20:44:46 -0700

    chore: add compose prod build
    - need to go update the server with dependencies

---
+++ scripts/compose-prod-build.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+/**
+ * System helper script that is used to compose a production build after the `client` and `server` builds have been completed.
+ *
+ * The target output of this will be a top level `dist` folder that includes
+ * the server code, prepared to run with a production build of the client code
+ * served as-is directly from the server.
+ */
+(async () => {
+  // TODO: Implement this script to copy the client build into the server's public folder and prepare the server for production deployment.
+})();
