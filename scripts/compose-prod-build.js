b56a053859bf3b0d2f18994e06624547af79dec0
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-16 22:07:33 -0700

    feat: finalize compose prod build

--- scripts/compose-prod-build.js
+++ scripts/compose-prod-build.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+import fs from "node:fs/promises";
 
 /**
  * System helper script that is used to compose a production build after the `client` and `server` builds have been completed.
@@ -8,11 +8,21 @@ const fs = require("fs");
  * served as-is directly from the server.
  */
 (async () => {
-  // TODO: Implement this script to copy the client build into the server's public folder and prepare the server for production deployment.
-  //
-  // for client:
-  // - move the client into the server's public folder
-  // for server:
-  // - mkdir /data/seeds
-  // - cp -r ../data/seeds ./data/seeds, need to make this optional
+  // **note** could be optimized, with parallelization, but not doing this for simplicity.
+  await fs.cp("./server/dist", "./dist", { recursive: true });
+  await fs.rm("./dist/public/index.html", { force: true });
+  await fs.cp("./client/dist", "./dist/public", { recursive: true });
+  await fs.mkdir("./dist/data/seeds", { recursive: true });
+  await fs.cp("./server/data/seeds", "./dist/data/seeds", { recursive: true });
+  await fs.cp("./server/package.json", "./dist/package.json");
+
+  // Update dist/package.json scripts to only include "start": "node main"
+  const distPkg = JSON.parse(await fs.readFile("./dist/package.json", "utf-8"));
+  distPkg.scripts = {
+    start: "node main",
+  };
+  await fs.writeFile(
+    "./dist/package.json",
+    JSON.stringify(distPkg, null, 2) + "\n",
+  );
 })();
