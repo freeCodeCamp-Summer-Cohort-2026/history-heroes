5918b0db095991b836ff2ae5689fe3b9b40814fd
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-17 21:03:50 -0700

    fix: uncaught promise in compose script

--- scripts/compose-prod-build.js
+++ scripts/compose-prod-build.js
@@ -8,21 +8,30 @@ import fs from "node:fs/promises";
  * served as-is directly from the server.
  */
 (async () => {
-  // **note** could be optimized, with parallelization, but not doing this for simplicity.
-  await fs.cp("./server/dist", "./dist", { recursive: true });
-  await fs.rm("./dist/public/index.html", { force: true });
-  await fs.cp("./client/dist", "./dist/public", { recursive: true });
-  await fs.mkdir("./dist/data/seeds", { recursive: true });
-  await fs.cp("./server/data/seeds", "./dist/data/seeds", { recursive: true });
-  await fs.cp("./server/package.json", "./dist/package.json");
+  try {
+    // **note** could be optimized, with parallelization, but not doing this for simplicity.
+    await fs.cp("./server/dist", "./dist", { recursive: true });
+    await fs.rm("./dist/public/index.html", { force: true });
+    await fs.cp("./client/dist", "./dist/public", { recursive: true });
+    await fs.mkdir("./dist/data/seeds", { recursive: true });
+    await fs.cp("./server/data/seeds", "./dist/data/seeds", {
+      recursive: true,
+    });
+    await fs.cp("./server/package.json", "./dist/package.json");
 
-  // Update dist/package.json scripts to only include "start": "node main"
-  const distPkg = JSON.parse(await fs.readFile("./dist/package.json", "utf-8"));
-  distPkg.scripts = {
-    start: "node main",
-  };
-  await fs.writeFile(
-    "./dist/package.json",
-    JSON.stringify(distPkg, null, 2) + "\n",
-  );
+    // Update dist/package.json scripts to only include "start": "node main"
+    const distPkg = JSON.parse(
+      await fs.readFile("./dist/package.json", "utf-8"),
+    );
+    distPkg.scripts = {
+      start: "node main",
+    };
+    await fs.writeFile(
+      "./dist/package.json",
+      JSON.stringify(distPkg, null, 2) + "\n",
+    );
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 })();
