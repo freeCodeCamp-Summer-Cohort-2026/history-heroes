c9510807a6dc4a36dac3d719552a2d62b9515768
Brad Taniguchi <bradtaniguchi@gmail.com>
2026-09-19 10:42:27 -0700

    feat: add ButtonLink component

---
+++ client/src/components/ButtonLink.tsx
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom'
+
+/**
+ * Button that redirects using react-router-dom, for internal application navigation.
+ *
+ * For native anchor tag usage, use "native" true, this should only be used to route to another page entirely
+ */
+export default function ButtonLink(
+  params:
+    | {
+        children: React.ReactNode
+        variant?: 'primary' | 'secondary'
+        className?: string
+        native: true
+        href: string
+      }
+    | {
+        children: React.ReactNode
+        variant?: 'primary' | 'secondary'
+        className?: string
+        native?: false
+        to: string
+      },
+) {
+  const { variant, native, children, className } = params
+  const variantClass =
+    variant === 'primary'
+      ? 'btn-primary hover:bg-[var(--color-primary-focus)]'
+      : 'btn-outline btn-secondary'
+
+  if (native) {
+    const { href } = params
+    return (
+      <a href={href} className={`btn ${variantClass} ${className}`}>
+        {children}
+      </a>
+    )
+  }
+
+  const { to } = params as {
+    to: string
+    children: React.ReactNode
+    variant?: 'primary' | 'secondary'
+    className?: string
+  }
+
+  return (
+    <Link to={to} className={`btn ${variantClass} ${className}`}>
+      {children}
+    </Link>
+  )
+}
