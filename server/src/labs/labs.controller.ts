32d3011055f412f414a9942f8a3dfeb9693149ac
shy_away <149800599+shy-away@users.noreply.github.com>
2026-09-18 21:59:35 -0500

    chore(api): add jsdocs for getLabByModuleId()

--- server/src/labs/labs.controller.ts
+++ server/src/labs/labs.controller.ts
@@ -5,6 +5,10 @@ import { LabsService } from './labs.service';
 export class LabsController {
   constructor(private readonly labsService: LabsService) {}
 
+  /**
+   * From a module ID, find its associated lab. The returned lab contains all of its activity data in the same response.
+   * @param moduleId The ID of the module associated with the returned lab
+   */
   @Get(':moduleId')
   public async getLabByModuleId(@Param('moduleId') moduleId: string) {}
 }
