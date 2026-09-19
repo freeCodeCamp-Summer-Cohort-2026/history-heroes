212e61fbe1b24432ae7e94011f145bd34f135132
shy_away <149800599+shy-away@users.noreply.github.com>
2026-09-18 21:57:18 -0500

    feat(api): build labs service structure

--- server/src/labs/labs.service.ts
+++ server/src/labs/labs.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Module } from '../modules/entities/module.entity';
+import { Activity } from '../activities/entities/activity.entity';
+import { Lab } from './entities/lab.entity';
 
 @Injectable()
-export class LabsService {}
+export class LabsService {
+  constructor(
+    @InjectRepository(Lab)
+    private readonly labsRepository: Repository<Lab>,
+    @InjectRepository(Module)
+    private readonly modulesRepository: Repository<Module>,
+    @InjectRepository(Activity)
+    private readonly activitiesRepository: Repository<Activity>,
+  ) {}
+
+  public async findByModuleId(moduleId: string) {
+    // find lab entity by module id
+
+    // find all associated activities
+  }
+}
