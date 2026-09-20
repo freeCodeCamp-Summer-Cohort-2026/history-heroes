3d8d0d5948f75782ba190c24ad48a6f9b1334921
Lorevdh <119044579+Lorevdh@users.noreply.github.com>
2026-09-20 16:44:56 +0200

    feat(client): add route for locked lesson page (#164)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import type { RouteObject } from 'react-router-dom'
 import LessonPage from './pages/LessonPage'
 import ModuleCatalogPage from './pages/ModuleCatalogPage'
 import ModulePage from './pages/ModulePage'
+import LockedLessonPage from './pages/LockedLessonPage'
 import NotFoundPage from './pages/NotFoundPage'
 import Layout from './components/Layout'
 import ComponentShowcase from './components/ComponentShowcase'
@@ -13,6 +14,7 @@ export const routes: RouteObject[] = [
       { path: '/', element: <ModuleCatalogPage /> },
       { path: '/modules/:moduleId', element: <ModulePage /> },
       { path: '/modules/:moduleId/lessons/:lessonId', element: <LessonPage /> },
+      { path: '/modules/:moduleId/locked', element: <LockedLessonPage /> },
       { path: 'components', element: <ComponentShowcase /> },
       { path: '*', element: <NotFoundPage /> },
     ],
