f41c9cbda14e1ae0c3d1f2065c79a91ca5f0d2ba
shy_away <149800599+shy-away@users.noreply.github.com>
2026-09-23 17:23:54 -0500

    fix(api): remove unnecessary vars

--- server/src/labs/labs.e2e-spec.ts
+++ server/src/labs/labs.e2e-spec.ts
@@ -1,31 +1,19 @@
 import { INestApplication } from '@nestjs/common';
-import { Lab } from './entities/lab.entity';
 import { App } from 'supertest/types';
-import { Repository } from 'typeorm';
 import { createTestApp } from '../../test/e2e-helper';
-import { getRepositoryToken } from '@nestjs/typeorm';
 import labsJson from '../../data/seeds/initial-labs.json';
 import request from 'supertest';
-import { Activity } from '../activities/entities/activity.entity';
 
 describe('Labs controller', () => {
   const seedLabs = labsJson.labs;
   let app: INestApplication<App>;
-  let labRepo: Repository<Lab>;
-  let activitiesRepo: Repository<Activity>;
 
   beforeEach(async () => {
     app = await createTestApp();
-    labRepo = app.get(getRepositoryToken(Lab));
-    activitiesRepo = app.get(getRepositoryToken(Activity));
   });
 
   afterEach(async () => {
-    if (app) {
-      await labRepo.clear();
-      await activitiesRepo.clear();
-      await app.close();
-    }
+    if (app) await app.close();
   });
 
   it('GET labs/:moduleId should return a lab with a populated activities array', async () => {
