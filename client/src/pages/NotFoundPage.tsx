3c2b501c69141c9b1e1419dc3181709c26b6b265
River <river.karnas@gmail.com>
2026-09-25 22:57:40 -0300

    feat(client): change back the heading so the app test doesn't break

--- client/src/pages/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import ButtonLink from '../components/ButtonLink'
 export default function NotFoundPage() {
   return (
     <div>
-      <h1 className="text-display">404</h1>
+      <h1 className="text-display">Page not found</h1>
       <p className="text-body">This page doesn't exist</p>
       <ButtonLink to={'/'}>Home Page</ButtonLink>
     </div>
