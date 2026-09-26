51e66dfb1d94328624fc2d88366a973ee03c8ed5
River <river.karnas@gmail.com>
2026-09-25 22:57:04 -0300

    feat(client): add test for unknown route reaching the page

--- client/src/pages/__tests__/NotFoundPage.test.tsx
+++ client/src/pages/__tests__/NotFoundPage.test.tsx
@@ -1,3 +1,12 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { createMemoryRouter, RouterProvider } from 'react-router-dom'
 import { routes } from '../../App'
+
+test(`tests uknown route reaching the page`, async () => {
+  render(
+    <RouterProvider
+      router={createMemoryRouter(routes, { initialEntries: ['/random'] })}
+    />,
+  )
+  expect(screen.getByRole('link', { name: 'Home Page' })).toBeInTheDocument()
+})
